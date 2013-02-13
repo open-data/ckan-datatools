3bf73ba7f1d9e098a2ea50e775e01cfb61416103
pjakobsen <pjakobsen@gmail.com>
2013-02-13 14:57:45 -0500

    Added module for reading last line of very large files without having to read it into memory with readlines

---
+++ src/batch/tools/filetools.py
@@ -0,0 +1,27 @@
+## {{{ http://code.activestate.com/recipes/578095/ (r1)
+import os
+import sys
+
+def print_first_last_line(inputfile) :
+    'gets the last line of a file without reading the entire file into memory'
+    filesize = os.path.getsize(inputfile)
+    blocksize = 1024
+    dat_file = open(inputfile, 'rb')
+    headers = dat_file.readline().strip()
+    if filesize > blocksize :
+        maxseekpoint = (filesize // blocksize)
+        dat_file.seek(maxseekpoint*blocksize)
+    elif filesize :
+        maxseekpoint = blocksize % filesize
+        dat_file.seek(maxseekpoint)    
+    lines =  dat_file.readlines()    
+    if lines :
+        last_line = lines[-1].strip()
+    print "first line : ", headers
+    print "last line : ", last_line
+
+if __name__ == "__main__" :
+    if len(sys.argv) >= 2:
+        print_first_last_line(sys.argv[1])
+    else:
+        sys.exit("Usage %s filename" % sys.argv[0])
