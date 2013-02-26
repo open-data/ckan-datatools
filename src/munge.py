006695085476218bc77c1d90038de91113d0db9e
Peder Jakobsen <pjakobsen@gmail.com>
2013-02-26 16:23:28 -0500

    Now using nested structures in argparse

--- src/munge.py
+++ src/munge.py
@@ -16,12 +16,12 @@ class Resourse:
 
 class DataImport:
     protocol = 'http://'
-    def __init__(self):
-        self.server = 'http://localhost:5000'
+    def __init__(self, server):
+        self.server = 'http://' + server
         self.api_base = "/api/action/"
         self.port = 5000
         self.debug_proxy = 'http://localhost:8888'
-        self.url =  self.protocol + self.top_domain + self.api_base
+        self.url =  self.server + self.api_base
         pass
     def test(self):
         ''' Creates a test package in CKAN, retrieves it to verify that it was entered, and then deletes it '''
@@ -34,11 +34,14 @@ class DataImport:
         print response
         
     def list_by_owner(self,owner):
+        print 'the owner is ' + owner
+        #package_list -d '{}'
         response = self.api3_call('package_list',{})
         for item in response:
             print item['name']
     
     def purge_by_owner(self,owner):
+        print 'the onwer is ' + owner
         #package_list -d '{}'
         pass
    
@@ -82,13 +85,38 @@ class DataImport:
 if __name__ == "__main__":
 
     import argparse
-    parser = argparse.ArgumentParser('GoC Open Data import tools.')
-    group = parser.add_argument_group()
-    parser.add_argument("command", help="Perform and operation on data", choices=['list','purge'])
-    parser.add_argument("-o", "--owner", help="ID of data owner")
+    parser = argparse.ArgumentParser('munge.py')
+    subparsers = parser.add_subparsers(help='commands')
+    
+    # Commands for reporting on existing data
+    report_parser = subparsers.add_parser('list', help='List Dataset Summary')
+    report_parser.add_argument('entity', action='store', help='Data entity type', choices=['pack','org','user'])
+    report_parser.add_argument('owner', action='store', help='System data owner',default='tester')
+
+    # Commands for creating new data
+    create_parser = subparsers.add_parser('make', help='Create New Data')
+    create_parser.add_argument('dirname', action='store', help='New directory to create')
+    create_parser.add_argument('--read-only', default=False, action='store_true',
+                           help='Set permissions to prevent writing to the directory',
+                           )
+    
+    # Commands for deleting and purging datasets
+    delete_parser = subparsers.add_parser('del', help='Remove a directory')
+    delete_parser.add_argument('dirname', action='store', help='The directory to remove')
+    delete_parser.add_argument('--recursive', '-r', default=False, action='store_true',
+                           help='Remove the contents of the directory, too',
+                           )
+
+    parser.add_argument("owner", help="Perform and operation on data")
+
     parser.add_argument("-s","--server", help="CKAN Server.  Default is localhost:5000", default="localhost:5000")
     parser.add_argument("-p","--proxy", help="Proxy for debugging etc. Default is None")
     parser.add_argument("-v", "--verbose", help="increase output verbosity")
+ 
     args = parser.parse_args()
-    if args.command == 'list':
-        DataImport().list_by_owner("tester")
+
+    print args.server
+
+    if args.entity == 'pack':
+        DataImport(args.server).list_by_owner(args.owner)
+   
