1b790386f00fb0c741c11abc235c9bf22aded88e
pjakobsen <pjakobsen@gmail.com>
2013-05-23 16:22:09 -0400

    New files for updating registry

---
+++ src/workflow_fields_fix.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import ckanapi
+#from jsonpath import jsonpath
+from pprint import pprint 
+import datatools
+
+def update_all_fields(ckansite):
+    print "Working with data at  ", ckansite, ":    "
+    pack_ids = datatools.registry_package_list()
+    registry = ckanapi.RemoteCKAN('http://registry.statcan.gc.ca/')
+    for i,pack_id in enumerate(pack_ids):
+        # 1. Get the package
+        pack = registry.action.package_show(id=pack_id)['result']
+        #pprint(pack)
+        pprint(pack['ready_to_publish'])
+        #pprint(pack['portal_release_date'])
+        # 2.  Update the fields
+        pack['ready_to_publish']=True
+        #pack['
+        #pprint(pack['ready_to_publish'])
+        # 3. Write the package
+        #ckan.logic.action.create.package_create(context, data_dict)
+        # 4. Get the package as pack_after
+        # 5. Check ensure evething is ok by comparing pack and pack_after
+        for key, value in pack.items():
+            print key
+            if key == "resources": continue
+            # deal with that pesky '\u2013' dash char
+            #print key, (u"%s" % value).encode('utf8') 
+        sys.exit() 
+ 
+
+if __name__ == "__main__":
+    #update_all_fields("http://data.statcan.gc.ca/data")
+    update_all_fields("http://registry.statcan.gc.ca")
