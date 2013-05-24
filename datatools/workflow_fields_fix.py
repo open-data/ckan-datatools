800d081fe8a0481a87a20ff144c19973f28551d5
unknown <jakoped@C205420.statcan.ca>
2013-05-24 17:41:26 -0400

    Trying to update packages with my own api and proxy

--- datatools/workflow_fields_fix.py
+++ datatools/workflow_fields_fix.py
@@ -68,26 +68,32 @@ def update_all_fields(ckansite):
     for i,pack_id in enumerate(pack_ids):
         # 1. Get the package
         pack = registry.action.package_show(id=pack_id)['result']
-        #pprint(pack)
+        pprint(pack['id'])
         pprint(pack['ready_to_publish'])
         #pprint(pack['portal_release_date'])
         # 2.  Update the fields
         pack['ready_to_publish']=True
+        pack['portal_relase_date'] = '2013-05-24'
+        if pack_id  in pending:
+            pack['portal_relase_date'] = ''
         #pack['
         #pprint(pack['ready_to_publish'])
         # 3. Write the package
-        #ckan.logic.action.create.package_create(context, data_dict)
+        resp = registry.action.package_update(**pack)
         # 4. Get the package as pack_after
+        #print resp
         # 5. Check ensure evething is ok by comparing pack and pack_after
+        '''
         for key, value in pack.items():
             print key
             if key == "resources": continue
             # deal with that pesky '\u2013' dash char
             #print key, (u"%s" % value).encode('utf8') 
+        '''
         sys.exit() 
  
 
 if __name__ == "__main__":
     #update_all_fields("http://data.statcan.gc.ca/data")
-    #update_all_fields("http://registry.statcan.gc.ca")
-    update_all_fields("http://localhost:5000")
+    update_all_fields("http://registry.statcan.gc.ca")
+    #update_all_fields("http://localhost:5000")
