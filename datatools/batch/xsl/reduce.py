04e292fd141d9e7d042913bf01da736cdfba2eeb
Peder Jakobsen <pjakobsen@gmail.com>
2013-07-10 11:44:21 -0400

    Combine and format 2 fields for notes and notes_fra

--- datatools/batch/xsl/reduce.py
+++ datatools/batch/xsl/reduce.py
@@ -109,11 +109,8 @@ def polygon(context, points):
 def make_notes(context,c1,c2):
     print c1[0]
     new = "\n".join([n.lstrip(":").replace("\n","").replace(":htt",": htt") for n in c2[0].split("\n:")])
-    print new
-    sys.exit()
-    return c1+c2
+    return c1 + new
     
-
 def clean_keywords(s): 
     return ",".join(set([n.strip() for n in s.split(",") if n.strip()]))
      
