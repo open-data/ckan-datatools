59720c9e7932abb2b83b871dccb912e1006311cf
pjakobsen <pjakobsen@gmail.com>
2013-07-16 16:12:55 -0400

    Better logging

--- datatools/registry/add_missing_pilot_fields.py
+++ datatools/registry/add_missing_pilot_fields.py
@@ -34,10 +34,9 @@ if __name__ == "__main__":
     subject_types=schema_description.dataset_field_by_id['subject']['choices_by_pilot_uuid']
     print subject_types
     subjects={}
-    resume_flag=True
+    resume_flag=False
     for child in root:
        try:
-          
            
            formid = child.xpath("FORM[NAME='thisformid']/A/text()")[0]
            if formid.lower()=='f80bbec0-0a48-4fe6-8efd-fa1b7bfbe6cb': resume_flag=True
@@ -62,21 +61,28 @@ if __name__ == "__main__":
     2. Change the package
     3. Update the package
     '''
+    
     print "---------- Updating -----------", len(subjects)
+    n=0
     for id,subject in subjects.iteritems():
-        print id.lower()
+        n+=1
         try:
             pack = registry.action.package_show(name_or_id=id.lower())
-            print ">>> ", pack['subject'], pack['catalog_type']
+            print n, id.lower(), ",",  pack['subject'], pack['catalog_type']
+            pack['catalog_type']=""
             if not pack['subject'] or not pack['catalog_type']: 
-                
+                print "yeahh"
+                sys.exit()
                 pack['subject'] = [subject]
                 pack['catalog_type'] = u"Data | Données"
                 result = registry.action.package_update(**pack)
                 pack = registry.action.package_show(name_or_id=id.lower())
-                print pack['subject']
-                print pack['catalog_type']
+              
+                print n, id.lower(), ", Updating", id, pack['subject'], pack['catalog_type']
         except:
-            print ("No record:  http://registry.statcan.gc.ca/en/dataset/%s" % id.lower())
+               
+                print n, id.lower(), ", No Record"
+                pass
+            #print ("No record:  http://registry.statcan.gc.ca/en/dataset/%s" % id.lower())
 
                         
